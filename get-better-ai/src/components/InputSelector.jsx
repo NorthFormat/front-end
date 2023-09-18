feaa4959ebbccf05a647c4aabd3ae4c4253f570c
EndS1end <Mr-Ender-Slender@yandex.ru>
2023-09-18 21:33:54 +0400

    Loader anim

--- get-better-ai/src/components/InputSelector.jsx
+++ get-better-ai/src/components/InputSelector.jsx
@@ -3,14 +3,29 @@ import BasicButton from '../components/BasicButton'
 import MicPic from '../assets/images/svg/mic.svg'
 import DocPic from '../assets/images/svg/document.svg'
 import CamPic from '../assets/images/svg/camera.svg'
+import Done from '../assets/images/svg/okay.svg'
+import Close from '../assets/images/svg/close.svg'
+import Equalizer from '../assets/images/svg/716.gif'
 
 function InputSelector() {
   return (
-    <div className='input-selector'>
-      <BasicButton imageSource={MicPic} />
-      <BasicButton imageSource={DocPic} />
-      <BasicButton imageSource={CamPic} />
-    </div>
+    <>
+      <div className='input-selector'>
+        <BasicButton imageSource={MicPic} />
+        <BasicButton imageSource={DocPic} />
+        <BasicButton imageSource={CamPic} />
+      </div>
+      <div className='input-selector'>
+        <BasicButton imageSource={Done} extraClass={'good'}/>
+        <img style={{height: '60%'}} src={Equalizer} alt="" />
+        <img style={{height: '60%'}} src={Equalizer} alt="" />
+        <img style={{height: '60%'}} src={Equalizer} alt="" />
+        <BasicButton imageSource={Close} extraClass={'bad'}/>
+      </div>
+      <div>
+        
+      </div>
+    </>
   );
 }
 
