23e5b4d393547240aabcb5c8b95d1d10c193670f
ZhuganPavel <Mr-Ender-Slender@yandex.ru>
2023-09-21 10:59:22 +0400

    Some css fixes

--- get-better-ai/src/services/FileService.js
+++ get-better-ai/src/services/FileService.js
@@ -18,7 +18,26 @@ export const upload = (file) => {
         .catch(error => console.log(error))
 }
 
+export const download = (file) => {
+    let data = new FormData()
+    data.append('file', file)
+
+    const options = {
+        method: 'POST',
+        body: data,
+        header: {'Access-Control-Allow-Origin': '*',}
+    };
+
+    return fetch(`${API_URL}/download`, options)
+        .then(response => response.text())
+        .then(response => {
+            return response
+        })
+        .catch(error => console.log(error))
+}
+
 export const decode = (byteArr) => {
     const decoder = new TextDecoder('utf-8');
     return decoder.decode(byteArr);
 }
+
